db54c980baea6697b1afd11a31389c8f5be88ad9
Jay Williams <course@jaywilliams.me>
2017-08-11 12:41:05 +0100

    Actually got their example working

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var config = require("./config.json");
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + config.file_locations.index);
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
 });
 
 http.listen(config.server.port, function(){
