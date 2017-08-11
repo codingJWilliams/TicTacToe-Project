bc064be3f02c22481466d9011f79463b0452994a
Jay Williams <course@jaywilliams.me>
2017-08-11 13:16:17 +0100

    Legwork done

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ var config = require("./config.json");
 var io = require('socket.io')(http, {'pingInterval': 3000, 'pingTimeout': 3000});
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + config.file_locations.index);
+  res.sendFile(__dirname + config.file_locations.lobby);
 });
+app.use('/static', require('express').static('static'))
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
