2b864e817e89de8339dfdce0a5f96b1606052c2c
Jay Williams <course@jaywilliams.me>
2017-08-11 14:59:15 +0100

    Moar Coolness

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ var http = require('http').Server(app);
 var config = require("./config.json");
 var io = require('socket.io')(http, {'pingInterval': 3000, 'pingTimeout': 3000});
 var rn = require('random-number');
-var mongo = new (require("mongo-sync").Server)(config.database.uri);
-var result = mongo.db("authentication").getCollection("users").find().toArray();
-console.log(result)
+var MongoClient = require('mongodb').MongoClient;
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + config.file_locations.lobby);
 });
@@ -15,14 +14,21 @@ app.use('/static', require('express').static('static'))
 function generateGameCode(){ return "TTT-" + gRD() + gRD() + gRD() + gRA() + gRA() + gRA() }
 function gRD(){ return String(rn({min:0, max:9, integer: true})) }
 function gRA(){ return String.fromCharCode(rn({min: 0, max:25, integer: true}) + 65) }
-io.on('connection', function(socket){
-  console.log('User Joined');
-  socket.on('please-join-game')
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-});
 
+MongoClient.connect(config.database.uri, function (err, db){
+  if(err) { return console.dir(err); }
+
+  io.on('connection', function(socket){
+    console.log('User Joined');
+    socket.on('please-join-game', function(params) {
+      if(!params.game.startswith("TTT")) return;
+      db.collec
+    })
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
+  });
+}
 http.listen(config.server.port, function(){
   console.log('listening on *:' + config.server.port);
 });
