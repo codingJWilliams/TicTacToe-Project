b88264f2625da9d633ae339eeb14c8d33b3512ff
Jay Williams <course@jaywilliams.me>
2017-08-11 14:36:30 +0100

    Added uri config

--- index.js
+++ index.js
@@ -3,27 +3,20 @@ var http = require('http').Server(app);
 var config = require("./config.json");
 var io = require('socket.io')(http, {'pingInterval': 3000, 'pingTimeout': 3000});
 var rn = require('random-number');
+var mongo = new (require("mongo-sync").Server)(config.database.uri);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + config.file_locations.lobby);
 });
-app.get("/newgame/", function(req, res){
-  console.log(req.query)
-  res.redirect('/play/?game=' + generateGameCode() + "&nickname=" + req.query.nickname);
-})
-app.get('/play/', function(req, res){
-  res.sendFile(__dirname + config.file_locations.play_friend);
-});
+app.get("/newgame/", function(req, res){ res.redirect('/play/?game=' + generateGameCode() + "&nickname=" + req.query.nickname); })
+app.get('/play/', function(req, res){ res.sendFile(__dirname + config.file_locations.play_friend); });
 app.use('/static', require('express').static('static'))
 function generateGameCode(){ return "TTT-" + gRD() + gRD() + gRD() + gRA() + gRA() + gRA() }
 function gRD(){ return String(rn({min:0, max:9, integer: true})) }
 function gRA(){ return String.fromCharCode(rn({min: 0, max:25, integer: true}) + 65) }
-
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+  console.log('User Joined');
+  socket.on('please-join-game')
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
