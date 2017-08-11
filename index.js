3110fb6f6a39376b3c21c594efd37b498d7fd619
Jay Williams <course@jaywilliams.me>
2017-08-11 14:00:31 +0100

    Url routing and game entry

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ var app = require('express')();
 var http = require('http').Server(app);
 var config = require("./config.json");
 var io = require('socket.io')(http, {'pingInterval': 3000, 'pingTimeout': 3000});
+var rn = require('random-number');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + config.file_locations.lobby);
 });
+app.get("/newgame/", function(req, res){
+  console.log(req.query)
+  res.redirect('/play/?game=' + generateGameCode() + "&nickname=" + req.query.nickname);
+})
 app.use('/static', require('express').static('static'))
+function generateGameCode(){ return "TTT-" + gRD() + gRD() + gRD() + gRA() + gRA() + gRA() }
+function gRD(){ return String(rn({min:0, max:9, integer: true})) }
+function gRA(){ return String.fromCharCode(rn({min: 0, max:25, integer: true}) + 65) }
 
 io.on('connection', function(socket){
   console.log('a user connected');
