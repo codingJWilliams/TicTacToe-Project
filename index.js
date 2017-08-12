b109f121d2350fa29e6799a9fcc21db2a948d943
Jay Williams <course@jaywilliams.me>
2017-08-12 21:23:23 +0100

    More documentation, more better :-)
    All my JS is commented.
    Optimized some code along the way as well so all is good

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var rn = require('random-number');
 var MongoClient = require('mongodb').MongoClient;
 
 app.get('/', function(req, res){ res.sendFile(__dirname + config.file_locations.lobby); }); // Serve up static "join game" page
-app.get("/newgame/", function(req, res){ res.redirect('/play/?game=' + generateGameCode() + "&nickname=" + req.query.nickname); }) // On request to /newgame/ make a game code. Could be client side TODO: Make client side
+app.get("/newgame/", function(req, res){ res.redirect('/play/?game=' + generateGameCode()); }) // On request to /newgame/ make a game code. TODO: Make client side?
 app.get('/play/', function(req, res){ res.sendFile(__dirname + config.file_locations.play_friend); }); // Serve up static file. No magic here
-app.use('/static', require('express').static('static'))
+app.use('/static', require('express').static(config.file_locations.static))
 
 function generateGameCode(){ return "TTT-" + gRD() + gRD() + gRD() + gRA() + gRA() + gRA() } // Return String game code where format is TTT-111AAA
 function gRD(){ return String(rn({min:0, max:9, integer: true})) }                     // Returns random 0-9 character
@@ -29,20 +29,20 @@ io.on('connection', function(socket){                                           
       MongoClient.connect(config.database.uri, function (err, db){              // Connect to database
         if(err) { return console.dir(err); }
         db.collection(config.database.collection, function(err, games){         // Get games collection as var games
-          games.find({code: params.game}).limit(1).toArray(function(err, docs) {
-            if(docs.length === 1){
-              var g = docs[0];
-              delete g['_id'];
-              g.player2 = socket.id;
-              games.updateOne({code: params.game}, {"$set": {"player2": socket.id}}, function (err, d){if (err) console.log(err);});
-              socket.emit("game-accepted", {database: g})
-            } else {
+          games.find({code: params.game}).limit(1).toArray(function(err, docs) {// Get the first game where the code is the one asked for, and return results as array
+            if(docs.length === 1){                                              // If anything was found, ie the current player is the second one (join)
+              var g = docs[0];                                                  // Save the result's object for convenience
+              delete g['_id'];                                                  // Delete the id object as it is not serializable
+              g.player2 = socket.id;                                            // Modify it with p2's id. This means that an admin can retrieve p2 from DB and p2 has their own id
+              games.updateOne({code: params.game}, {"$set": {"player2": socket.id}}, function (err, d){if (err) console.log(err);}); // Update game in DB with player 2's id
+              socket.emit("game-accepted", {database: g})                       // Tell the second player that they can communicate with p1, and include p1s id
+            } else {                                                            // If nothing was found, eg this player is the host
               var g = {
-                code: params.game,
-                player1: socket.id
+                code: params.game,                                              // Make an object with the game code in it and send it to player
+                player1: socket.id                                              // Tell it it's own ID
               }
-              socket.emit("game-accepted", {database: g})
-              games.insertOne(g)
+              socket.emit("game-accepted", {database: g})                       // Tell it the game was accepted. By fact that the p2 wasn't included, it knows it is the host and waits on p2
+              games.insertOne(g)                                                // Finally, add game to DB so p2 can look it up
             }
           });
         });
@@ -53,4 +53,4 @@ io.on('connection', function(socket){                                           
   });
 });
 
-http.listen(config.server.port, function(){ console.log('HTTP Server is listening on *:' + config.server.port); });
+http.listen(config.server.port, function(){ console.log('HTTP Server is listening on *:' + config.server.port); }); // Start de whole shebangs
